bb954b763bcb89b564e2f58e3ecaaf96efd108f7
Roy Plomantes <rplomantes@gmail.com>
2025-11-24 15:00:23 +0800

    fix version3

--- version.php
+++ version.php
@@ -27,5 +27,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'enrol_nephilazip';
 $plugin->release = '1.0.1 for Moodle 4.5';
 $plugin->version = 2025112405;    // New version number, higher than previous
-$plugin->requires = 2025081100;   // Minimum Moodle 4.5
+$plugin->requires = 2025111800;   // Minimum Moodle 4.5
 $plugin->maturity = MATURITY_STABLE;
