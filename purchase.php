238210ae7e156f69c941bccb593a6d960e6bf8b4
Roy Plomantes <rplomantes@gmail.com>
2025-11-24 15:10:20 +0800

    fix version5

--- purchase.php
+++ purchase.php
@@ -4,10 +4,8 @@ require_login();
 
 global $USER, $DB;
 
-// Get course ID from URL
 $courseid = required_param('id', PARAM_INT);
 
-// Get course and enrol instance
 $course = $DB->get_record('course', ['id'=>$courseid], '*', MUST_EXIST);
 $instance = $DB->get_record('enrol', [
     'enrol' => 'nephilazip',
@@ -15,22 +13,27 @@ $instance = $DB->get_record('enrol', [
     'status' => 1
 ]);
 
-$amount = $instance->cost ?? 0;
+if (!$instance) {
+    print_error('No Nephila Zip enrolment instance found for this course.');
+}
 
-// Get base URL from plugin settings
+$amount = floatval($instance->cost);
+if ($amount <= 0) {
+    print_error('Course cost must be greater than zero.');
+}
+
 $baseurl = get_config('enrol_nephilazip', 'baseurl');
+if (empty($baseurl)) {
+    print_error('Base URL for Nephila Zip is not configured.');
+}
 
-// Dynamic client reference
 $client_reference_id = "course{$courseid}_user{$USER->id}";
 
-// Construct checkout URL
 $checkouturl = $baseurl . '/checkout?' . http_build_query([
     'client_reference_id' => $client_reference_id,
-    'amount' => intval($amount * 100), // in cents if required
+    'amount' => intval($amount * 100),
     'email' => $USER->email,
     'name' => $USER->firstname . ' ' . $USER->lastname,
 ]);
 
-// Redirect user to checkout
 redirect($checkouturl);
-
